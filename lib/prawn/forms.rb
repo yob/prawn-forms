909c8b86a8bb4a8f8c080796216c688ec6445fa9
Brad Ediger <brad.ediger@madriska.com>
2010-07-29 10:22:51 +0800

    Add text field flags

--- lib/prawn/forms.rb
+++ lib/prawn/forms.rb
@@ -21,10 +21,11 @@ module Prawn
 
     def text_field(name, x, y, w, h, opts = {})
       x, y = map_to_absolute(x, y)
+
       field_dict = {:T => Prawn::Core::LiteralString.new(name),
                     :DA => Prawn::Core::LiteralString.new("/Helv 0 Tf 0 g"),
                     :F => 4,
-                    :Ff => 0,
+                    :Ff => flags_from_options(opts),
                     :BS => {:Type => :Border, :W => 1, :S => :S},
                     :MK => {:BC => [0, 0, 0]},
                     :Rect => [x, y, x + w, y + h]}
@@ -64,6 +65,33 @@ module Prawn
                   :BaseFont => :Helvetica,
                   :Encoding => :WinAnsiEncoding)
       ref!(:Font => {:Helv => helv})
+    end
+
+    # Returns the integer value for the /Ff (flags) entry in the field
+    # dictionary, based on the options provided.
+    #
+    def flags_from_options(opts)
+      flags = 0
+
+      flags |= 1<<12 if opts[:multiline]
+      flags |= 1<<13 if opts[:password]
+
+      if opts[:file_select]
+        min_version 1.4
+        flags |= 1<<20
+      end
+
+      if opts[:do_not_spell_check]
+        min_version 1.4
+        flags |= 1<<22
+      end
+
+      if opts[:do_not_scroll]
+        min_version 1.4
+        flags |= 1<<23
+      end
+
+      flags
     end
   end
 end
